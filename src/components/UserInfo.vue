82c4f6e78534b5eb4a709d83d67db262d6ce1bfe
Adannaio <udohmercy911@gmail.com>
2021-11-24 11:36:00 +0000

    first commit

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="user-info-container">
+      <div>
+        <span>{{userInfo.name}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['userInfo'],
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,124 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <div class="main-container">
+          <h1 style="font-family:'Roboto', sans-serif; color:#130f40">Devfinder</h1>
+          <form class="search-box" >
+            <input class="search-text" type="text" name="Search User" placeholder="Search User" v-model="username">
+            <button class="search-btn" type="button" @click="getUserInfo"><i class="fas fa-search"></i></button>
+          </form>
+          <UserInfo :userInfo="userInfo"  />
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import UserInfo from "../components/UserInfo.vue";
+import {ref} from "vue"; 
+import axios from "axios";
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { UserInfo },
+
+  setup() {
+    const username = ref("");
+    const userInfo =  ref(null);
+
+    const getUserInfo = async () => {
+      try {
+        const response = await axios.get(`https://api.github.com/users/${username.value}`);
+        const result = response.data;
+        console.log(result);
+        userInfo.value = {
+          image:result.avatar_url,
+          name: result.name,
+          user_name: result.username,
+          joined: result.created_at,
+          bio: result.bio,
+          repos: result.public_repos,
+          followers: result.followers,
+          following: result.following,
+          location: result.location,
+          twitter: result.twitter_username,
+          blog:result.blog,
+          company: result.company
+        }
+        
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    return {
+      username,
+      userInfo,
+      getUserInfo
+    }
   }
 }
 </script>
+
+<style scoped>
+  
+
+  .main-container{
+    margin: 50px auto;
+    padding:45px;
+    width: 700px;
+    height:300px;
+    color: black;
+  }
+
+  h4{
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .search-box{
+    background: #30336b;
+    height: 30px;
+    border-radius: 40px;
+    padding: 10px;
+    width:600px;
+  }
+
+  .search-btn{
+    color: #535c68;
+    float: right;
+    width:27px;
+    height: 27px;
+    border-radius: 50%;
+    background: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.4s;
+    cursor: pointer;
+    border: none;
+  }
+
+  .search-btn:hover{
+    color:  white;
+    background: #30336b;
+  }
+
+  .search-text{
+    border: none;
+    background: none;
+    outline: none;
+    float: left;
+    padding: 0;
+    color: white;
+    font-size: 16px;
+    transition: 0.4s;
+    line-height: 28px;
+    padding: 0, 6px;
+  }
+
+  .search-text::placeholder{
+    color:#535c68;
+  }
+
+</style>
+
+
+<style>
+
+</style>
